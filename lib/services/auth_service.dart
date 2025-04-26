159f86f4c43381d90e2a02308669ed3ba7c2ef2e
Amjad-Tp <amjadtpvalakkai@gmail.com>
2025-04-26 16:26:58 +0530

    Removed some bugs in google sign-in, (auth_service.dart)

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -45,7 +45,11 @@ class AuthService {
 
   // Google Sign In
   Future<void> signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    await googleSignIn.signOut();
+
+    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
 
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
@@ -57,44 +61,4 @@ class AuthService {
 
     await FirebaseAuth.instance.signInWithCredential(credential);
   }
-
-  // // Facebook Sign In
-  // Future<User?> signInWithFacebook() async {
-  //   final LoginResult result = await FacebookAuth.instance.login();
-
-  //   if (result.status == LoginStatus.success) {
-  //     final OAuthCredential credential = FacebookAuthProvider.credential(
-  //       result.accessToken!.token,
-  //     );
-  //     final userCredential = await _auth.signInWithCredential(credential);
-  //     return userCredential.user;
-  //   }
-
-  //   return null;
-  // }
-
-  // // Twitter Sign In
-  // Future<User?> signInWithTwitter() async {
-  //   final twitterLogin = TwitterLogin(
-  //     apiKey: 'YOUR_TWITTER_API_KEY',
-  //     apiSecretKey: 'YOUR_TWITTER_SECRET_KEY',
-  //     redirectURI: 'YOUR_TWITTER_REDIRECT_URI',
-  //   );
-
-  //   final authResult = await twitterLogin.login();
-
-  //   if (authResult.status == TwitterLoginStatus.loggedIn) {
-  //     final twitterAuthCredential = TwitterAuthProvider.credential(
-  //       accessToken: authResult.authToken!,
-  //       secret: authResult.authTokenSecret!,
-  //     );
-
-  //     final userCredential = await _auth.signInWithCredential(
-  //       twitterAuthCredential,
-  //     );
-  //     return userCredential.user;
-  //   }
-
-  //   return null;
-  // }
 }
